94fa9552756dec0ef0a6a4cbc25550590c054159
Thomas Misson <11tmisson@gmail.com>
2019-04-19 00:12:55 +0100

    Resolved issue with returning promise

--- Node Mongo db connection/operations_mongo_db.js
+++ Node Mongo db connection/operations_mongo_db.js
@@ -5,15 +5,15 @@ MongoClient.connect(url, { useNewUrlParser: true },  function(err, db) {
     if (err) throw err;
     var dbo = db.db("E-portfolio");
 
-    dbo.collection("Qualifications").insert(
+    /*dbo.collection("Qualifications").insertOn(
       {"Course":"Fundamentals of Digital Marketing",
       "Type":"Accreditation",
       "Issuer":"Open university & Google",
       "Issue date":"2019-01-19T23:00:00.000Z",
       "Certificate number":"7KB 6RC HDV"})
-
+*/
+    let res = Promise.resolve(dbo.collection("Qualifications").find().count()).then(function (res){
+    console.log("You have registered " + res + " qualifications");});
+    
     db.close();
-    
-    console.log("You have registered " + dbo.collection("Qualifications").find().count() + " qualifications");
-    
 });
