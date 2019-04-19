6915bbe8920964bea209eebc753c8326a674108e
Thomas Misson <11tmisson@gmail.com>
2019-04-19 13:43:35 +0100

    changed to let rather than var

--- Node Mongo db connection/create_mongo_db.js
+++ Node Mongo db connection/create_mongo_db.js
@@ -1,9 +1,9 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/e-portfolio";
+let MongoClient = require('mongodb').MongoClient;
+let url = "mongodb://localhost:27017/e-portfolio";
 
 MongoClient.connect(url, { useNewUrlParser: true },  function(err, db) {
   if (err) throw err;
-  var dbo = db.db("E-portfolio");
+  let dbo = db.db("E-portfolio");
   dbo.createCollection("Qualifications", function(err, res) {
     if (err) throw err;
     console.log("Collection created!");
